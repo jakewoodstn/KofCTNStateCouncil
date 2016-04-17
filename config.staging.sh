8fb905a8e15309571651feece6977483fa80485b
jakewoodstn <jake.woods@comcast.net>
2016-04-17 18:15:25 +0000

    tuning changes to staging deployment script

--- config.staging.sh
+++ config.staging.sh
@@ -3,3 +3,8 @@ cp config.core.php.staging config.core.php
 cp manager/config.core.php.staging manager/config.core.php
 cp connectors/config.core.php.staging connectors/config.core.php
 cp core/config/config.inc.php.staging core/config/config.inc.php
+mkdir core/cache
+mkdir core/export
+mkdir core/import
+chmod 777 core/cache
+chmod -R 777 assets/components
